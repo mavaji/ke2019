3cdda001eee893f61f5d5ce149fa3e38f0b99af5
Vahid Mavaji <mavaji@outlook.com>
2019-06-17 11:09:44 +0430

    add Scala applicative

---
+++ scala/src/P06Applicative.scala
@@ -0,0 +1,30 @@
+object P06Applicative {
+  def main(args: Array[String]): Unit = {
+    val some2 = Some(2)
+    val some3 = Some(3)
+
+    val fmapResult = OptionFunctor fmap(f, some2)
+    println(fmapResult)
+
+    val applyResult = OptionApplicative apply(fmapResult, some3)
+    println(applyResult)
+  }
+
+  val f: Int => Int => Int = x => y => x + y
+}
+
+trait Applicative[F[_]] {
+  def apply[A, B](f: F[A => B], fa: F[A]): F[B]
+}
+
+object OptionApplicative extends Applicative[Option] {
+  override def apply[A, B](f: Option[A => B], fa: Option[A]): Option[B] = {
+    fa match {
+      case None => None
+      case Some(a) => f match {
+        case None => None
+        case Some(g) => Some(g(a))
+      }
+    }
+  }
+}
